007438389ad01f8fd9efcf7ed81e28792e12eb4c
JeisonMenaSistec <jmena@sisteccr.com>
2026-04-14 10:05:38 -0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,70 @@
+const CACHE_NAME = 'mi-app-v1.5';
+const URLS_A_CACHEAR = [
+  '/',
+  '/index.html',
+  '/anexo16.html',
+  '/anexo17.html',
+  '/manifest.webmanifest',
+  '/logo192.jpg',
+  '/logo512.jpg'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(URLS_A_CACHEAR);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+self.addEventListener('fetch', (event) => {
+  const isNavigate = event.request.mode === 'navigate';
+
+  if (isNavigate) {
+    event.respondWith(
+      fetch(event.request)
+        .then((networkResponse) => {
+          return caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, networkResponse.clone());
+            return networkResponse;
+          });
+        })
+        .catch(() => caches.match(event.request))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((respuestaCache) => {
+      const fetchPromise = fetch(event.request)
+        .then((networkResponse) => {
+          return caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, networkResponse.clone());
+            return networkResponse;
+          });
+        })
+        .catch(() => respuestaCache);
+
+      return respuestaCache || fetchPromise;
+    })
+  );
+});
